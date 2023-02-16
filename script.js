f65148c84475d11ae037f3206985c87df3906ac4
karan__ <114335802+jaskaran281@users.noreply.github.com>
2023-02-16 16:51:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const featureBtn = document.querySelector(".features");
+const companyBtn = document.querySelector(".company");
+
+
+const bodyDocumnet = document.querySelector("html");
+
+
+const featureMenu = document.querySelector(".drop-down-menu");
+const companyMenu = document.querySelector(".drop-down-menu-2");
+
+function toggleChange (x) {
+    x.classList.toggle("change");
+}
+
+
+
+
+
